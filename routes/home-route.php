09d068b02e3dbab3bf8c58e61a12dad9cfd5a44c
abraham-ukachi <abraham.ukachi@laplateforme.io>
2023-04-11 09:31:45 +0200

    :green_heart: Create home & shop routes

---
+++ routes/home-route.php
@@ -0,0 +1,95 @@
+<?php
+/**
+* @license MIT
+* boutique-en-ligne (maxaboom)
+* Copyright (c) 2023 Abraham Ukachi, Axel Vair, Morgane Marechal, Catherine Tranchand. The Maxaboom Project Contributors.
+* All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* @project boutique-en-ligne
+* @name Home - Route
+* @file home-route.php
+* @author: Abraham Ukachi <abraham.ukachi@laplateforme.io>
+* @contributors: Axel Vair <axel.vair@laplateforme.io>, Morgane Marechal <morgane.marechal@laplateforme.io>, Catherine Tranchand <catherine.tranchand@laplateforme.io>
+* @version: 0.0.1
+* 
+* Usage:
+*   1+|> //  
+*    -|> 
+*/
+
+
+/*
+* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+* MOTTO: We'll always do more 😜!!!
+* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+
+
+// declare the `routes` namespace
+namespace Maxaboom\Routes;
+
+// use maxaboom's `APIController` class
+use Maxaboom\Controllers\HomeController;
+
+
+
+/** 
+ * Home - Route 
+ */
+
+
+/**
+ * Route used to display the splash, welcome or home view
+ * 
+ * @method GET
+ * @action /
+ *
+ * @echo string $splashScreen|$welcomeScreen|$homePage - the splash screen, welcome screen or home page
+ */
+$router->map('GET', '/', function(): void {
+  // get the home controller
+  $homeController = new HomeController();
+  
+  // show the splash screen
+  // $homeController->showSplashScreen();
+
+  // show the home page
+  $homeController->showHomePage();
+
+});
+
+
+/**
+ * Route used to redirect to the home page
+ *
+ * @method GET
+ * @action /home
+ * @redirect / - the home page
+ */
+$router->map('GET', '/home', function() {
+  // redirect to the home page
+  header("Location: " . MAXABOOM_HOME_DIR);
+});
+
+
+
+
+
---
+++ routes/shop-route.php
@@ -0,0 +1,83 @@
+<?php
+/**
+* @license MIT
+* boutique-en-ligne (maxaboom)
+* Copyright (c) 2023 Abraham Ukachi, Axel Vair, Morgane Marechal, Catherine Tranchand. The Maxaboom Project Contributors.
+* All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* @project boutique-en-ligne
+* @name Shop - Route
+* @file shop-route.php
+* @author: Abraham Ukachi <abraham.ukachi@laplateforme.io>
+* @contributors: Axel Vair <axel.vair@laplateforme.io>, Morgane Marechal <morgane.marechal@laplateforme.io>, Catherine Tranchand <catherine.tranchand@laplateforme.io>
+* @version: 0.0.1
+* 
+* Usage:
+*   1+|> //  
+*    -|> 
+*/
+
+
+/*
+* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+* MOTTO: We'll always do more 😜!!!
+* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+
+
+// declare the `routes` namespace
+namespace Maxaboom\Routes;
+
+
+// use maxaboom's `ShopController` class
+use Maxaboom\Controllers\ShopController;
+
+
+
+/** 
+ * Home - Route 
+ */
+
+
+/**
+ * Route used to display the splash, welcome or home view
+ * 
+ * @method GET
+ * @url /shop
+ *
+ * @echo string $shopPage - the shop page
+ */
+$router->map('GET', '/shop', function(): void {
+  // instantiate the `ShopController` class as `$shopController`
+  $shopController = new ShopController();
+  
+  // TODO: do something awesome here before showing the shop page ;)
+
+  // show the shop page
+  $shopController->showPage();
+  
+
+});
+
+
+
+
+
