69cec58fc456a34039aaf91695b40c374c77e0c2
abraham-ukachi <abraham.ukachi@laplateforme.io>
2023-04-11 09:26:17 +0200

    :racehorse: Include external home, shop & api routes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
 * @license MIT
 * boutique-en-ligne (maxaboom)
-* Copyright (c) 2023 Abraham Ukachi, Axel Vair, Morgane Marechal. The Maxaboom Project Contributors.
+* Copyright (c) 2023 Abraham Ukachi, Axel Vair, Morgane Marechal, Catherine Tranchand. The Maxaboom Project Contributors.
 * All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -24,29 +24,16 @@
 * SOFTWARE.
 *
 * @project boutique-en-ligne
-* @name Home Page - Maxaboom
+* @name Main / Router - Maxaboom
 * @file index.php
 * @author: Abraham Ukachi <abraham.ukachi@laplateforme.io>
-* @contributors: Axel Vair <axel.vair@laplateforme.io>, Morgane Marechal <morgane.marechal@laplateforme.io>
+* @contributors: Axel Vair <axel.vair@laplateforme.io>, Morgane Marechal <morgane.marechal@laplateforme.io>, Catherine Tranchand <catherine.tranchand@laplateforme.io>
 * @version: 0.0.1
 * 
 * Usage:
-*   1-|> open http://localhost/boutique-en-ligne/index.php
-* 
+*   1+|> // how-to route with AltoRouter ;)
+*    -|>
 *
-* ============================
-*     >>> DESCRIPTION <<<
-* ~~~~~~~~ (French) ~~~~~~~~~
-* 
-* -  Votre entreprise décide de mettre en place une boutique en ligne (Thème et produits au choix).
-*
-* ~~~~~~~~ (English) ~~~~~~~~~
-* 
-* - Your company decides to set up an online shop (Choose your theme and products).
-* 
-* ============================
-* IMPORTANT: This is a working progress and subject to major changes ;)
-* ============================
 */
 
 
@@ -56,151 +43,61 @@
 * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 */
 
-
-?><!DOCTYPE html>
-    
-<!-- HTML -->
-<html lang="en">
-
-  <!-- HEAD -->
-  <head>
-    <!-- Our 4 VIP metas -->
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=no">
-    <meta name="description" content="Maxaboom is a fun and dynamic online store that offers a wide variety of musical instruments. From guitars and drums to keyboards, microphones and trumpets.">
-    
-    <!-- Title -->
-    <title>Welcome to maxaboom | The #1 online store for all your musical needs</title>
+// ---===--- enabling error reporting ---====---
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+// ---===---===---===---===---===---===---===---
 
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <!-- Mulish - Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;700&display=swap" rel="stylesheet">
-    
-    <!-- Material Icons - https://github.com/google/material-design-icons/tree/master/font -->
-    <!-- https://material.io/resources/icons/?style=baseline -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  
-     
-    <!-- Base -->
-    <base href="/boutique-en-ligne/">
-
-    <!-- Logo - Icon -->
-    <link rel="icon" href="assets/images/favicon.ico">
-
-    <!-- See https://goo.gl/OOhYW5 -->
-    <link rel="manifest" href="manifest.json">
-
-    <!-- See https://goo.gl/qRE0vM -->
-    <meta name="theme-color" content="#FFDCBA">
-
-    <!-- Add to homescreen for Chrome on Android. Fallback for manifest.json -->
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="application-name" content="Maxaboom">
-
-    <!-- Add to homescreen for Safari on iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="default">
-    <meta name="apple-mobile-web-app-title" content="Maxaboom">
-
-    <!-- Homescreen icons -->
-    <link rel="apple-touch-icon" href="assets/images/manifest/icon-48x48.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="assets/images/manifest/icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="96x96" href="assets/images/manifest/icon-96x96.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="assets/images/manifest/icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="192x192" href="assets/images/manifest/icon-192x192.png">
+// require the autoloader
+require __DIR__ . '/vendor/autoload.php';
 
 
-    <!-- Theme -->
-    <link rel="stylesheet" href="assets/theme/color.css">
-    <link rel="stylesheet" href="assets/theme/typography.css">
-    <link rel="stylesheet" href="assets/theme/styles.css">
-    
-    
-    <!-- Animations -->
-    <!-- <link rel="stylesheet" href="assets/animations/fade-in-animation.css"> -->
-    <!-- <link rel="stylesheet" href="assets/animations/slide-from-down-animation.css"> -->
-
-    <!-- Stylesheet -->
-    <!-- <link rel="stylesheet" href="assets/stylesheets/home-styles.css"> -->
 
 
-    <!-- Script -->
-    <script>
+// defining some constants...
 
-      // Let's do some stuff when this page loads...
-      window.addEventListener('load', (event) => { 
-        // ...do something awesome here ;)
-      });
-      
-    </script>
-    
-    <!-- Some more script for ya! #LOL -->
-    <script src="src/app.js" type="module" defer></script>
-    <!-- <script src="src/script/home.js" defer></script> -->
-    
-  </head>
-  <!-- End of HEAD -->
-  
-  
-  <!-- BODY | Default Theme: light -->
-  <body class="theme light" fullbleed>
-
-    <!-- MAIN -->
-    <main class="flex-layout vertical">
-      <!-- App Layout - MAIN -->
-      <div class="app-layout"></div>
-      <!-- End of App Layout - MAIN -->
-
-      <!-- Nav Bar -->
-      <!-- PHP: Include the `nav-bar` component -->
-      <?php 
-        $_GET['navbar_orientation'] = 'veritcal'; 
-        $_GET['navbar_page'] = 'home'; 
-        $_GET['navbar_connected'] = 'false'; 
-      ?>
-
-      <?php // include 'components/nav-bar.php'; ?>
-      <!-- End of Nav Bar -->
-
-    </main>
-    <!-- End of MAIN -->
-
-    
-
-    <!-- ASIDE -->
-    <aside class="flex-layout vertical" hidden>
-      <!-- Vertical Left - DIVIDER -->
-      <span class="divider vertical left"></span>
-
-      <!-- App Layout - ASIDE -->
-      <div class="app-layout" fit></div>
-      <!-- End of App Layout - ASIDE -->
-    </aside>
-    <!-- End of ASIDE -->
-    
-
-    <!-- Backdrop -->
-    <div id="backdrop" hidden></div>
+// home or base directory of Maxaboom
+const MAXABOOM_HOME_DIR = '/boutique-en-ligne';
 
 
-    <!-- Menus  -->
-    <div id="menus" hidden></div>
-    <!-- End of Menus  -->
 
-    <!-- Dialogs  -->
-    <div id="dialogs" hidden></div>
-    <!-- End of Dialogs  -->
 
-    <!-- Toast -->
-    <div id="toast" hidden></div>
-    <!-- End of Toast -->
 
-  </body>
-  <!-- End of BODY -->
 
-</html>
-<!-- End of HTML -->
+
+
+// Create an altorouter instance named `$router`
+$router = new AltoRouter();
+
+// Set the base path of the router
+$router->setBasePath(MAXABOOM_HOME_DIR);
+
+
+
+// Now, let's add some routes to our router ;)
+
+
+// Include these routes
+include __DIR__ . '/routes/home-route.php'; // <- home route
+include __DIR__ . '/routes/shop-route.php'; // <- shop route
+include __DIR__ . '/routes/api-route.php'; // <- api route
+
+
+
+
+// match the current request url
+$match = $router->match();
+
+
+// call closure or throw 404 status
+if(is_array($match) && is_callable($match['target'])) {
+  call_user_func_array($match['target'], $match['params']);
+} else {
+  // TODO: Create a 404 view / page to handle this
+
+	// no route was matched
+	header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
+
+
