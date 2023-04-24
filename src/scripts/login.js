ca88dd876847094899aaaf1a606128e9b8146bb6
axelvair <axel.vair@laplateforme.io>
2023-04-24 09:13:58 +0200

    change form login

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -16,10 +16,8 @@ async function handleFormSubmit(event) {
     let request = new Request(url, {method: "POST", body: form});
     let response = await fetch(request);
     let responseData = await response.json();
-    checkInputs();
     if (responseData.success) {
         mbApp.showToast({message: "La connexion a réussi !", type: SUCCESS_TOAST}, 2);
-
         setTimeout(() => {
             location.replace("shop");
         }, 2000);
@@ -27,44 +25,3 @@ async function handleFormSubmit(event) {
 };
 
 formConnect.addEventListener('submit', handleFormSubmit);
-
-let login = document.querySelector('.login-connect');
-let password = document.querySelector('.password-connect');
-
-
-/**
- * Function that checks inputs value and launch functions for display the errors
- */
-function checkInputs() {
-    const loginValue = login.value.trim();
-    const passwordValue = password.value.trim();
-
-    if (loginValue === '') {
-        setErrorFor(login, 'Le login doit faire au moins 3 caractères');
-    } else if (loginValue.length >= 3) {
-        setSuccessFor(login)
-    }
-    if (passwordValue === '' || passwordValue.length < 3) {
-        setErrorFor(password, 'Le mot de passe doit faire au moins 3 caractères');
-    } else if (passwordValue.length >= 3) {
-        setSuccessFor(password)
-    }
-}
-
-function setErrorFor(input, message){
-    const inputWrapper = input.parentElement; // .input-wrapper
-    const small = formControl.querySelector('small');
-
-    //add error message inside small tag
-    small.innerText = message;
-
-    //add error class
-    inputWrapper.className = 'input-wrapper error';
-}
-
-function setSuccessFor(input, message) {
-    const inputWrapper = input.parentElement;
-    inputWrapper.className = 'input-wrapper success';
-}
-
-
