7d96d0e8c3f4d0f8b8e89a049446f70d619a9142
abraham-ukachi <abraham.ukachi@laplateforme.io>
2023-06-19 21:56:54 +0200

    :fire: Revamp Order Model with Abstract and Interface

--- models/Order.php
+++ models/Order.php
@@ -25,7 +25,7 @@
 *
 * @project boutique-en-ligne (maxaboom)
 * @name Order - Model
-* @test test/order.php
+* @test test/order_model.php
 * @file Database.php
 * @author: Abraham Ukachi <abraham.ukachi@laplateforme.io>
 * @contributors: Axel Vair <axel.vair@laplateforme.io>, Morgane Marechal <morgane.marechal@laplateforme.io>, Catherine Tranchand <catherine.tranchand@laplateforme.io>
@@ -33,303 +33,210 @@
 * 
 * Example usage:
 *   
-*   1+|> // 
+*   1+|> // Generate a new order id
 *    -|>
+*    -|> $generatedOrderId = Order::generateId();
+*    -|>
+*    -|> echo $generatedOrderId; // 163456789
+*
+*
+*   2+|> // Create a new order 
+*    -|>
+*    -|> $order = Order::create([
+*    -|>   'id' => $generatedOrderId,
+*    -|>   'user_id' => 1,
+*    -|>   'status' => 'pending',
+*    -|>   'address_id' => 1,
+*    -|>   'card_id' => 1,
+*    -|>   'delivery_method' => 'standard',
+*    -|>   'payment_method' => 'visa',
+*    -|>   'total' => 100.00,
+*    -|>   'discount_percentage' => 0,
+*    -|>   'total_discounted' => 100.00,
+*    -|>   'tax_amount' => 20.00,
+*    -|>   'delivery_amount' => 10.00,
+*    -|>   'total_price' => 130.00,
+*    -|> ]);
+*
 *
  */
 
 
-
+// declare the namespace for this `Order` class
 namespace Maxaboom\Models;
 
-use Maxaboom\Models\Helpers\Database;
+// use these classes
 use datetime;
-use pdo, PDOException;
 
 
-class Order extends Database
-{
-    /*
-        One order is composed of :
-            -one number of order
-            -one $id_user
-            -one total price
-            -one create time
-            -one paid time
-            -some products
-            -one address
-            -one delivery type
-    */
-
-    private ?int $numberOfOrder = null;
-    private ?int $id_user = null;
-    private ?int $price = null;
-    private ?DateTime $createTime = null;
-    private ?DateTime $paidTime = null;
-    private ?string $products = null;
-    private ?string $address = null;
-    private ?string $delivery = null;
 
 
-    // private ?object $db = null;
-    private ?object $product = null;
-
-    public function __construct($product)
-    {
-        parent::__construct();
-        // $this->setDatabaseUsername('root');
-        // $this->setDatabasePassword('root');
-        // $this->setPort(8888);
-
-        // connect to the database
-        $this->dbConnect();
-
-        // initialize the `product` variable
-        $this->product = $product;
-    }
-
-    /**
-     * Method used to create and order with the given `userId` and `data`.
-     *
-     * @param int $id_user - the id of the user
-     * @param array $data - a list of all the necessary data to create an order
-     *
-     * @return false|array - Returns TRUE if the order has been created successfully
-     */
-    public function createOrder(int $id_user, array $data): false|array
-    {
-        // initialize the `result` variable
-        $result = ['success' => 'false', 'data' => []];
-
-        // do nothing if there's no user id or if it is equal to -1
-        if ($id_user === -1) {
-            return false;
-        }
-
-        /**
-         * IDEA:
-         *  1) - Complete the payment process w/ the payment info
-         *  2) - Insert a row in the `orders` table
-         *  3) - Add all the products into the `order_items` table
-         */
-
-        $created_at = date('Y-m-d h:i:s');
-        $paid_at = date('Y-m-d h:i:s');;
-
-        echo $created_at;
-
-        try {
-            // IDEA #2
-//            $query = "INSERT INTO orders(`user_id`, `created_at`, `paid_at`, `total_price`) VALUES (':user_id',':created_at',':paid_at',':total_price')";
-            $query = "INSERT INTO orders(`user_id`, `created_at`, `paid_at`, `total_price`) VALUES (?, ?, ?, ?)";
-
-            /* We need to have class Database */
-            $pdo_stmt = $this->db->prepare($query);
-            // bind the params
-//            $pdo_stmt->bindParam(':user_id', $id_user);
-//            $pdo_stmt->bindParam(':created_at', $created_at);
-//            $pdo_stmt->bindParam(':paid_at', $paid_at);
-//            $pdo_stmt->bindParam(':total_price', $total_price);
-
-            // execute the pdo statement
-            $pdo_stmt->execute(array($id_user, $created_at, $paid_at, $data['totalPrice']));
-
-            $rowCount = $pdo_stmt->rowCount();
-
-            echo "Row Count =>>>> $rowCount" . "<br>";
 
 
-            $result['success'] = 'true';
-
-            // get the order id
-            $randomOrderId = $this->generateOrderId();
-
-            // TODO: IDEA #3
-
-            echo $randomOrderId;
-
-            $result['data'] = ['randomOrderId' => $randomOrderId, 'userId' => $id_user];
 
 
-            //        foreach ($data['products'] as $productId => $productQty) {
-            //            // get the product detail using the `productId`
-            //            $products[] = $this->product->getProductById($productId);
-            //
-            //            }
-
-        } catch (PDOException $e) {
-            // handle the pdo exception
-            echo $e->getMessage();
-            die();
-
-        }
 
 
-        return $result;
-
-    }
 
 
-    /**
-     * Generates a time-based random order id.
-     *
-     * @return int
-     */
-    private function generateOrderId(): int
-    {
-        // get current timestamp
-        $timestamp = time();
-        // convert timestamp from binary to hexadecimal value & stringify the number
-        $hexTimestamp = strval(bin2hex($timestamp));
 
-        // IDEA: Use the last 9 numbers from `hexTimestamp` as the randomly generated order id
-        return (int)substr($hexTimestamp, -9, 9);
+/**
+ * Class Order / Order Model
+ * A class that represents the `orders` table in the database.
+ */
+class Order extends Model {
 
-    }
+  // Define some constants here ;)
 
-    /* Getters and setters of my properties */
 
-    /**
-     * @return int|null
-     */
-    public function getNumberOfOrder(): ?int
-    {
-        return $this->numberOfOrder;
-    }
 
-    /**
-     * @param int|null $numberOfOrder
-     * @return Product
-     */
-    public function setNumberOfOrder(?int $numberOfOrder): Product
-    {
-        $this->numberOfOrder = $numberOfOrder;
-        return $this;
-    }
+  // Define some properties here ;)
 
-    /**
-     * @return int|null
-     */
-    public function getIdUser(): ?int
-    {
-        return $this->id_user;
-    }
 
-    /**
-     * @param int|null $id_user
-     * @return Product
-     */
-    public function setIdUser(?int $id_user): Product
-    {
-        $this->id_user = $id_user;
-        return $this;
-    }
+  // protected properties
 
-    /**
-     * @return int|null
-     */
-    public function getPrice(): ?int
-    {
-        return $this->price;
-    }
 
-    /**
-     * @param int|null $price
-     * @return Product
-     */
-    public function setPrice(?int $price): Product
-    {
-        $this->price = $price;
-        return $this;
-    }
+  /**
+   * The table associated with this model
+   *
+   * @var string
+   */
+  protected string $table = 'orders';
 
-    /**
-     * @return DateTime|null
-     */
-    public function getCreateTime(): ?DateTime
-    {
-        return $this->createTime;
-    }
 
-    /**
-     * @param DateTime|null $createTime
-     * @return Product
-     */
-    public function setCreateTime(?DateTime $createTime): Product
-    {
-        $this->createTime = $createTime;
-        return $this;
-    }
+  /**
+   * Indicates if the model should automatically connect to the database.
+   *
+   * @var bool
+   */
+  protected bool $autoConnect = true;
 
-    /**
-     * @return DateTime|null
-     */
-    public function getPaidTime(): ?DateTime
-    {
-        return $this->paidTime;
-    }
 
-    /**
-     * @param DateTime|null $paidTime
-     * @return Product
-     */
-    public function setPaidTime(?DateTime $paidTime): Product
-    {
-        $this->paidTime = $paidTime;
-        return $this;
-    }
+  /**
+   * All the supported fields in the `orders` table
+   * @var array
+   */
+  protected array $fields = [
+    'id',
+    'user_id',
+    'status',
+    'address_id',
+    'card_id',
+    'delivery_method',
+    'payment_method',
+    'total',
+    'discount_percentage',
+    'total_discounted',
+    'tax_amount',
+    'delivery_amount',
+    'total_price',
+    'created_at'
+  ];
 
-    /**
-     * @return string|null
-     */
-    public function getProduct(): ?string
-    {
-        return $this->product;
-    }
 
-    /**
-     * @param string|null $product
-     * @return Product
-     */
-    public function setProduct(?string $product): Product
-    {
-        $this->product = $product;
-        return $this;
-    }
+  // public properties
 
-    /**
-     * @return string|null
-     */
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
+  public ?int $id = null;
+  public ?int $user_id = null;
+  public ?string $status = null;
+  public ?int $address_id = null;
+  public ?int $card_id = null;
+  public ?string $payment_method = null;
+  public ?float $total = null;
+  public ?int $discount_percentage = null;
+  public ?float $total_discounted = null;
+  public ?int $tax_amount = null;
+  public ?float $delivery_amount = null;
+  public ?float $total_price = null;
+  public ?string $created_at = null;
 
-    /**
-     * @param string|null $address
-     * @return Product
-     */
-    public function setAddress(?string $address): Product
-    {
-        $this->address = $address;
-        return $this;
-    }
 
-    /**
-     * @return string|null
-     */
-    public function getDelivery(): ?string
-    {
-        return $this->delivery;
-    }
+  /**
+   * Indicates if the model should be timestamped.
+   *
+   * @var bool
+   */
+  public bool $timestamps = true;
 
-    /**
-     * @param string|null $delivery
-     * @return Product
-     */
-    public function setDelivery(?string $delivery): Product
-    {
-        $this->delivery = $delivery;
-        return $this;
-    }
+  
+
+  // private properties
+
+  
+
+
+
+
+
+
+  /**
+   * Order constructor.
+   * NOTE: This constructor is called automatically when the class is instantiated
+   */
+  public function __construct() {
+    // call the parent Model constructor
+    parent::__construct();
+  }
+
+
+  // PUBLIC STATIC METHODS
+  
+  /**
+   * Generates a time-based unique random order id.
+   * NOTE: This generated id doesn't exist in the database yet.
+   *
+   * @return int
+   */
+  public static function generateId(): int {
+    // get current timestamp
+    $timestamp = time();
+    // convert timestamp from binary to hexadecimal value & stringify the number
+    $hexTimestamp = strval(bin2hex($timestamp));
+
+    // IDEA: Use the last 9 numbers from `hexTimestamp` as the randomly generated order id
+    return (int)substr($hexTimestamp, -9, 9);
+  }
+
+
+  // PUBLIC STATIC GETTERS
+
+  // PUBLIC STATIC SETTERS
+
+
+
+  // PUBLIC METHODS
+
+  // PUBLIC GETTERS
+
+  // PUBLIC SETTERS
+
+
+
+
+
+
+  // PRIVATE STATIC METHODS
+  
+  // PRIVATE STATIC GETTERS
+
+  // PRIVATE STATIC SETTERS
+  
+  
+  
+  // PRIVATE METHODS  
+
+  /**
+   * Checks if the given `orderId` already exists in this table
+   *
+   * @param int $orderId - the order id to check
+   *
+   * @return bool - returns TRUE if the order id exists, otherwise returns FALSE
+   * @private
+   */
+  private function verifyId(int $orderId): bool {}
+  
+   
+  // PRIVATE GETTERS
+
+  // PRIVATE SETTERS
 }
