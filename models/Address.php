6f3910e8e37ce8b2f60b7fd89666770fea91c94b
abraham-ukachi <abraham.ukachi@laplateforme.io>
2023-06-20 10:48:42 +0200

    :fire: Revamp Address Model with Abstract and Interface

--- models/Address.php
+++ models/Address.php
@@ -1,19 +1,193 @@
 <?php
+/*
+* @license MIT
+* boutique-en-ligne (maxaboom)
+* Copyright (c) 2023 Abraham Ukachi, Axel Vair, Morgane Marechal, Catherine Tranchand. The Maxaboom Project Contributors.
+* All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* @project boutique-en-ligne (maxaboom)
+* @name Address - Model
+* @test test/address_model.php
+* @file Address.php
+* @author: Axel Vair <axel.vair@laplateforme.io>
+* @contributors: Abraham Ukachi <abraham.ukachi@laplateforme.io>, Morgane Marechal <morgane.marechal@laplateforme.io>, Catherine Tranchand <catherine.tranchand@laplateforme.io>
+* @version: 0.0.1
+* 
+* Example usage:
+*   
+*   1+|> // Create a new address
+*    -|>
+*    -|> $address = Address::create([
+*    -|>   'title' => 'Home',
+*    -|>   'address' => '1 rue de la paix',
+*    -|>   'address_complement' => 'Appartement 1',
+*    -|>   'city' => 'Paris',
+*    -|>   'postal_code' => 75000,
+*    -|>   'country' => 'France',
+*    -|>   'user_id' => 1
+*    -|> ]);
+*    -|>
+*
+ */
 
+// declare the namespace for this `Address` class
 namespace Maxaboom\Models;
 
-use Maxaboom\Models\Helpers\Database;
+// use these classes
 use datetime;
 use PDO;
 use PDOException;
 
-class Address extends Database
-{
-    public function __construct()
-    {
-        parent::__construct();
-        $this->dbConnect();
-    }
+
+
+
+
+
+
+
+/**
+ * Class Address / Address Model
+ * A class that represents the `addresses` table in the database.
+ */
+class Address extends Model {
+
+  // Define some constants here ;)
+
+
+  // Define some properties here ;)
+
+
+  // protected properties
+
+
+  /**
+   * The table associated with this model
+   *
+   * @var string
+   */
+  protected string $table = 'addresses';
+
+
+  /**
+   * Indicates if the model should automatically connect to the database.
+   *
+   * @var bool
+   */
+  protected bool $autoConnect = true;
+
+
+  /**
+   * All the supported fields in the `addresses` table
+   * @var array
+   */
+  protected array $fields = [
+    'id',
+    'title',
+    'address',
+    'address_complement',
+    'postal_code',
+    'city',
+    'country',
+    'user_id',
+
+    'created_at',
+    'deleted_at'
+  ];
+
+
+  // public properties
+
+  public ?int $id = null;
+  public ?string $title = null;
+  public ?string $address = null;
+  public ?string $address_complement = null;
+  public ?int $postal_code = null;
+  public ?string $city = null;
+  public ?string $country = null;
+  public ?int $user_id = null;
+  // timestamps
+  public ?string $created_at = null;
+  public ?string $deleted_at = null;
+
+  
+  /**
+   * Indicates if the model should be timestamped.
+   *
+   * @var bool
+   */
+  public bool $timestamps = true;
+
+
+  // private properties
+
+
+
+
+  /**
+   * Address constructor.
+   * NOTE: This constructor is called automatically when the class is instantiated
+   */
+  public function __construct() {
+    // call the parent Model constructor
+    parent::__construct();
+  }
+  
+
+  // PUBLIC STATIC METHODS
+
+  // PUBLIC STATIC GETTERS
+
+  // PUBLIC STATIC SETTERS
+
+
+
+  // PUBLIC METHODS
+
+  // PUBLIC GETTERS
+
+  // PUBLIC SETTERS
+
+
+
+
+  // PRIVATE STATIC METHODS
+  
+  // PRIVATE STATIC GETTERS
+
+  // PRIVATE STATIC SETTERS
+  
+  
+  
+  // PRIVATE METHODS  
+
+  // PRIVATE GETTERS
+
+  // PRIVATE SETTERS
+
+
+
+
+
+
+  // +++++++ `Axel Vair` - CODE ++++++++
 
     public function getAll(int $userId){
         $addressUser = $this->db->prepare("SELECT * FROM addresses WHERE user_id=$userId");
@@ -25,7 +199,7 @@ class Address extends Database
 
     public function getOne(int $userId, int $addressId){
         $addressUser = $this->db->prepare("SELECT * FROM addresses WHERE user_id=$userId AND id = $addressId");
-        $addressUser->execute([
+$addressUser->execute([
         ]);
         $result = $addressUser->fetch(PDO::FETCH_ASSOC);
         return $result;
@@ -40,7 +214,7 @@ class Address extends Database
             'title' => htmlspecialchars($title),
             'address' => htmlspecialchars($address),
             'address_complement' => htmlspecialchars($complement),
-            'postal_code' => $postal_code,
+'postal_code' => $postal_code,
             'city' => htmlspecialchars($city),
             'country' => htmlspecialchars($country),
             'user_id' => $user_id,
@@ -48,7 +222,7 @@ class Address extends Database
         ]);         
         if ($sql_exe) {
            return true;
-        } else {
+} else {
            return false;
         }
     }
@@ -76,7 +250,7 @@ class Address extends Database
         } else {
             return false;
         }
-    }
+}
     //UPDATE TYPE ADDRESS
 
     function updateTypeAddress($idAddress, $newtype){
@@ -106,4 +280,9 @@ class Address extends Database
             }
 
     }
+
+
+
+    // +++++++ End of `Axel Vair` - CODE ++++++++
+
 }
